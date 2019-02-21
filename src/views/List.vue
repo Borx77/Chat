9062192bab91ee61de1b382a5ccd5644bd28d8e6
root <root@DESKTOP-I1RLS17.localdomain>
2019-02-21 15:02:15 +0100

    Add page list

---
+++ src/views/List.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="list">
+    <router-link to="/">Home</router-link>
+    <div class="w-full bg-grey-dark py-6">
+      <h1 class="text-center text-white">Chat de Amigos</h1>
+    </div>
+    <div class="bg-white-light w-full flex justify-center">
+      <div type="text" class="w-5/6 bg-grey-lighter m-3 p-2">
+        <i class="fas fa-search"></i>
+        <input>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "list",
+  components: {}
+};
+</script>
+
+<style lang="postcss" scoped>
+</style>
+
