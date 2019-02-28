f7d0fb962a4e610915a7f69a1dd1ec7ac071d450
root <root@DESKTOP-I1RLS17.localdomain>
2019-02-28 12:14:40 +0100

    Casi

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,18 +1,28 @@
 <template>
-  <div class="login">
-    <h1 class="text-center">Login</h1>
-    <select class="my-5" v-model="emisorSeleccionado">
-      <option value="0">Selecciona tu usuario</option>
-      <option
-        v-for="usuario in usuarios"
-        :key="usuario.id"
-        :value="usuario.id"
-      >{{ usuario.fields.Username }}</option>
-    </select>
-    <router-link
-      v-if="emisorSeleccionado != 0"
-      :to="{ name: 'list', params: {emisor: emisorSeleccionado }}"
-    >Entrar</router-link>
+  <div class="login fuente">
+    <h1 class="text-center pb-1">Login</h1>
+    <div class="fondo flex justify-center pt-24">
+      <div class="flex-col">
+        <div class="pb-24">
+          <select class="my-5 selector" v-model="emisorSeleccionado">
+            <option value="0">Selecciona tu usuario</option>
+            <option
+              v-for="usuario in usuarios"
+              :key="usuario.id"
+              :value="usuario.id"
+            >{{ usuario.fields.Username }}</option>
+          </select>
+        </div>
+        <div class="text-center">
+          <router-link
+            v-if="emisorSeleccionado != 0"
+            :to="{ name: 'list', params: {emisor: emisorSeleccionado }}"
+          >
+            <span class="boton-entrar">Entrar</span>
+          </router-link>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,5 +65,39 @@ export default {
 </script>
 
 <style lang="postcss" scoped>
+.fondo {
+  background: #c39146;
+  background: -moz-linear-gradient(left, #c39146 0%, #6dcaa3 100%);
+  background: -webkit-gradient(
+    left top,
+    right top,
+    color-stop(0%, #c39146),
+    color-stop(100%, #6dcaa3)
+  );
+  background: -webkit-linear-gradient(left, #c39146 0%, #6dcaa3 100%);
+  background: -o-linear-gradient(left, #c39146 0%, #6dcaa3 100%);
+  background: -ms-linear-gradient(left, #c39146 0%, #6dcaa3 100%);
+  background: linear-gradient(to right, #c39146 0%, #6dcaa3 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#c39146', endColorstr='#6dcaa3', GradientType=1 );
+  height: 96vh;
+}
+
+.selector {
+  width: 14rem;
+  height: 2rem;
+}
+.boton-entrar {
+  background-color: black;
+  color: white;
+  padding: 0.5rem;
+  padding-left: 1.2rem;
+  padding-right: 1.2rem;
+  border: 2px solid black;
+  border-radius: 5px;
+}
+
+.fuente {
+  font-family: "PT Sans Narrow", sans-serif;
+}
 </style>
 
